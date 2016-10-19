618079b83631ea9a8c8aa34eed0fe9b5ff586b9c
Miguel Pedrero <miguel.servimarket@gmail.com>
2016-10-18 21:42:54 -0300

    batman versus superman

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def batman
   end
+  def superman
+  end
+  def batman_vs_superman
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  get 'pages/batman_vs_superman'
   get 'pages/batman'
   get 'pages/superman'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'pages#batman_vs_superman'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
