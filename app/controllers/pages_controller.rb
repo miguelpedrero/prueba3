4340f934aa69cd42c330b5824366f830b8cee186
Miguel Pedrero <miguel.servimarket@gmail.com>
2016-10-18 22:14:17 -0300

    ultmo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,8 @@ class PagesController < ApplicationController
   def superman
   end
   def batman_vs_superman
-  	@vbatan = User.all
-  	# @vsuperman = User.all
+
   end
-    def save_user
-  	User.create({ name: params[:name], email: params[:email],]})
-  	redirect_to pages_batman_vs_superman_path
-  	end
+
+
 end
